82d9ad2cfd0a0084e579c9be7c335215fb70603b
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-01 19:57:17 -0300

    Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.css";
+import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const history = useHistory();
+
+    function login() {
+        alert("Bem vindo!\n" + email);
+        history.push("home");
+    }
     
     return(
-        <div>
-            <h1>Login</h1>
-            <h1>Login</h1>
-            <Link to="home">Home</Link>
-        </div>
+        
+    <div className="base">
+         <div className="container">
+             <form className="inputs">
+                <div className="titleConfig"><h1>LOGIN</h1></div>
+                <Form.Group controlId="email">
+                     <Form.Control 
+                        type="email" 
+                        placeholder="email" 
+                        onChange={(e) => setEmail(e.target.value)}
+                     />
+                </Form.Group>
+                <Form.Group controlId="password">
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </Form.Group>
+                <Button className="loginConfig" variant="dark" onClick={login}>Login</Button>{' '}
+                <Link className="linkConfig" to="cadastro">Cadastre-se já</Link>
+             </form>
+         </div>
+    </div>
     )
 }
 
