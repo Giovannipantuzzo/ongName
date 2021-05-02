26ff6e6c107fe0144fe6e81b5126e1c1e3fef413
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-02 19:02:34 -0300

    Início da home
    alterações na formatação do login

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,41 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import {
+    AppBar,
+    Toolbar,
+ } from "@material-ui/core";
 
 function Home() {
     const history = useHistory();
 
     return(
         <div>
-            <h1>Home</h1>
-            <button onClick={() => {
-                history.push("login");
-            }}>
-                Login
-            </button>
+            <AppBar position="static">
+                <Toolbar className="toolBarConfig">
+                        <img className="logo" src="/images/Logo.png" alt="Logo"></img>
+                        <button className="adocao" onClick={() => {
+                            history.push("adocao_animal");
+                        }}>
+                            Adoção de animais
+                        </button>
+                        <button onClick={() => {
+                            history.push("adote-ja");
+                        }}>
+                            Adote já
+                        </button>
+                        <button onClick={() => {
+                            history.push("login");
+                        }}>
+                            Login
+                        </button>
+                        <button onClick={() => {
+                            history.push("cadastro");
+                        }}>
+                            Cadastre-se
+                        </button>
+                </Toolbar>
+            </AppBar>
+          
         </div>
     )
 }
