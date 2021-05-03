a2572770f7768bd8bfba5ef1c9f7f69ca4236a55
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-03 13:52:01 -0300

    navBar
    foi adicionada a navBar, porém precisa configurar a visualização, principalmente para telefone, e foi formatado a tela de login, que também precisa ser formatada para o centro

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -4,33 +4,42 @@ import {
     AppBar,
     Toolbar,
  } from "@material-ui/core";
+ import "./Menu.css";
 
 function Menu() {
     const history = useHistory();
 
     return(
         <div>               
-                <img className="logo" src="/images/Logo.png" alt="Logo"></img>
-                        <button className="adocao" onClick={() => {
-                            history.push("adocao_animal");
-                        }}>
-                            Adoção de animais
-                        </button>
-                        <button onClick={() => {
-                            history.push("adote_ja");
-                        }}>
-                            Adote já
-                        </button>
-                        <button onClick={() => {
-                            history.push("login");
-                        }}>
-                            Login
-                        </button>
-                        <button onClick={() => {
-                            history.push("cadastro");
-                        }}>
-                            Cadastre-se
-                        </button>
+               <AppBar position="static">
+                <Toolbar style={{ backgroundColor: "#EDFE26", height: "6rem"}}>
+                        <img className="logo" src="/images/Logo.png" alt="Logo" onClick={() => {
+                                history.push("home");
+                            }}></img>
+                        <div className="buttonsSpace1">
+                            <button className="adocao" onClick={() => {
+                                history.push("adocao_animal");
+                            }}>
+                                Adoção de animais
+                            </button>
+                            <button className="adote" onClick={() => {
+                                history.push("adote-ja");
+                            }}>
+                                Adote já
+                            </button>
+                        </div>
+                        <div className="buttonsSpace2">
+                            <button className="Login" onClick={() => {
+                                history.push("login");
+                            }}>
+                                Login</button>{' '}
+                            <button className="cadastre" onClick={() => {
+                                history.push("cadastro");
+                            }}>
+                                Cadastre-se</button>{' '}
+                        </div>
+                </Toolbar>
+            </AppBar>
         </div>
     );
     
