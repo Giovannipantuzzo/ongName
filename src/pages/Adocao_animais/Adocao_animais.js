0b1b82c94176f3d84d3ac6830463317520f481e1
Matheus Franklin <matheusfranklin@cpejr.com.br>
2021-05-03 20:46:21 -0300

    Card primeira tentativa

--- src/pages/Adocao_animais/Adocao_animais.js
+++ src/pages/Adocao_animais/Adocao_animais.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Adocao_animais.css";
+import { Form, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Adocao_animais() {
-    return(
-        <div>
-            <h1>Adocao_animais</h1>
-        </div>
-    )
+  const history = useHistory();
+
+  function adotar() {
+    history.push("animal");
+  }
+
+  return (
+    <div className="base1">
+      <Card style={{ width: "18rem",cursor:"pointer"}} onClick={()=>adotar()}>
+        <Card.Img variant="top" src="/images/CachorroRosa.jpg" />
+        <Card.Body>
+          <Card.Title>Adote Pink</Card.Title>
+          <Card.Text>
+            Belo Horizonte - MG
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Adocao_animais;
