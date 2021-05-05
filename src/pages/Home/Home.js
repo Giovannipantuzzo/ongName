b41fc10d4f50df8e89799d119dc03a16d250a40b
Tainan Albuquerque <tainanalbuquerque1@hotmail.com>
2021-05-05 15:03:42 -0300

    carrossel
    carrossel de imagens sem os textos sobre imagens.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,17 @@ import {
     AppBar,
     Toolbar,
  } from "@material-ui/core";
+ import { Carousel } from "react-bootstrap";
+ import Card from 'react-bootstrap/Card';
+ import "./Home.css";
+
 
 function Home() {
     const history = useHistory();
 
     return(
-        <div>
+        
+        <>
             <AppBar color="red" position="static">
                 <Toolbar >
                         <img className="logo" src="/images/Logo.png" alt="Logo"></img>
@@ -35,9 +40,44 @@ function Home() {
                         </button>
                 </Toolbar>
             </AppBar>
-          
-        </div>
-    )
+            
+            <Carousel fade >
+                <Carousel.Item style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
+                    <img
+                    className="d-block w-100"
+                    src="./images/dogHome1.png"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <Card.Text>
+                        </Card.Text>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="./images/dogHome2.png"
+                    alt="Second slide"
+                    />
+                    
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="./images/dogHome3.png"
+                    alt="Third slide"
+                    />
+                     <Carousel.Caption>
+                        <Card.Text>
+                        
+                        </Card.Text>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            
+        </>
+        
+    );
 }
 
 export default Home;
