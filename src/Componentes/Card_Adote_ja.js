af5500dc92ef97aa95d910963f188eb149cb45bc
Gabriel Paschoalin <gabrieldias@cpejr.com.br>
2021-05-05 14:14:32 -0300

    Formatação do card

--- src/Componentes/Card_Adote_ja.js
+++ src/Componentes/Card_Adote_ja.js
@@ -1,45 +1,56 @@
-import React from "react"
-import "./componentes.css"
+import React from "react";
+import "./componentes_adote_ja.css";
 import { Card } from "react-bootstrap";
-import { BiCurrentLocation, BiMaleSign, BiFemaleSign } from 'react-icons/bi';
+import { BiCurrentLocation, BiMaleSign, BiFemaleSign } from "react-icons/bi";
 import { useHistory } from "react-router";
 
 function Produto({ animal }) {
+  const history = useHistory();
+  function Sexo() {
+    if (animal.sexo === "Male") return <BiMaleSign />;
+    else return <BiFemaleSign />;
+  }
+  function telaAnimal() {
+    history.push("animal");
+  }
 
-    const history = useHistory();
-    function Sexo() {
-        if (animal.sexo === "Male") return <BiMaleSign />;
-
-        else return <BiFemaleSign />
-    }
-    function telaAnimal() {
-        history.push("animal");
-    }
-
-    return (
-        <Card style={{ width: '18rem', cursor: "pointer", borderRadiusTop: "20px" }} onClick={() => telaAnimal()} className="Shadow">
-                <Card.Img   variant="top" src={animal.imagem} alt="Exemplo" />
-            <Card.Body>
-                <div className="Border">
-                    <Card.Title align="center"> Adote {animal.nome}</Card.Title>
-                    <Card.Text align="center">Tempo de abrigo: {animal.tempo}</Card.Text>
-                    <div className="infoRow">
-                        <p className="Cidade">
-                            <BiCurrentLocation />
-                            {animal.local}
-                        </p>
-                        <div className="SexoAdote_Ja" >
-                            <p>{animal.tamanho} |</p>
-                            {Sexo()}
-                        </div>
-                    </div>
-                    <Card.Text align="center">{animal.descricao}</Card.Text>
-                </div>
-            </Card.Body>
-        </Card >
-    );
+  return (
+    <Card
+      style={{ width: "18rem", cursor: "pointer" }}
+      onClick={() => telaAnimal()}
+      className="Shadow"
+    >
+      <Card.Img
+        variant="top"
+        src={animal.imagem}
+        alt="Exemplo"
+        style={{ borderRadius: "30px" }}
+      />
+      <Card.Body style={{ padding: "5px" }}>
+        <div className="Border">
+          <p align="center" className="tituloAdote_ja">
+            Adote {animal.nome}
+          </p>
+          <p align="center" className="tempoAbrigo">
+            Tempo de abrigo: {animal.tempo}
+          </p>
+          <div className="infoRow">
+            <p className="Cidade">
+              <BiCurrentLocation />
+              {animal.local}
+            </p>
+            <div className="SexoAdote_Ja">
+              <p>{animal.tamanho} |</p>
+              {Sexo()}
+            </div>
+          </div>
+          <p className="description" align="center">
+            {animal.descricao}
+          </p>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
-
 export default Produto;
-
--- src/pages/Adote_ja/Adote_ja.js
+++ src/pages/Adote_ja/Adote_ja.js
@@ -3,76 +3,106 @@ import "./Adote.css";
 import Produto from "../../Componentes/Card_Adote_ja";
 
 const animais = [
-    {
-        id: 1,
-        imagem: "/images/dog1.png",
-        nome: "Billy",
-        tempo: "2 anos",
-        local: "Belo Horizonte/MG",
-        tamanho: "M",
-        sexo: "Male",
-        descricao: "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
-    },
-    {
-        id: 2,
-        imagem: "/images/dog2.jpg",
-        nome: "Pluto",
-        tempo: "2 anos",
-        local: "Belo Horizonte/MG",
-        tamanho: "P",
-        sexo: "Female",
-        descricao: "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
-    },
-    {
-        id: 3,
-        imagem: "/images/dog1.png",
-        nome: "Billy",
-        tempo: "2 anos",
-        local: "Belo Horizonte/MG",
-        tamanho: "G",
-        sexo: "Male",
-        descricao: "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
-    },
-    {
-        id: 4,
-        imagem: "/images/dog2.jpg",
-        nome: "Pluto",
-        tempo: "2 anos",
-        local: "Belo Horizonte/MG",
-        tamanho: "P",
-        sexo: "Female",
-        descricao: "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
-    },
-    {
-        id: 5,
-        imagem: "/images/dog1.png",
-        nome: "Billy",
-        tempo: "3 anos",
-        local: "Belo Horizonte/MG",
-        tamanho: "P",
-        sexo: "Male",
-        descricao: "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
-    },
-    {
-        id: 6,
-        imagem: "/images/dog2.jpg",
-        nome: "Pluto",
-        tempo: "0,5 anos",
-        local: "Belo Horizonte/MG",
-        tamanho: "P",
-        sexo: "Female",
-        descricao: "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
-    },
+  {
+    id: 1,
+    imagem: "/images/dog1.png",
+    nome: "Billy",
+    tempo: "2 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "M",
+    sexo: "Male",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 2,
+    imagem: "/images/dog2.jpg",
+    nome: "Pluto",
+    tempo: "2 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "P",
+    sexo: "Female",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 3,
+    imagem: "/images/dog1.png",
+    nome: "Billy",
+    tempo: "2 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "G",
+    sexo: "Male",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 4,
+    imagem: "/images/dog2.jpg",
+    nome: "Pluto",
+    tempo: "2 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "P",
+    sexo: "Female",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 5,
+    imagem: "/images/dog1.png",
+    nome: "Billy",
+    tempo: "3 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "P",
+    sexo: "Male",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 6,
+    imagem: "/images/dog2.jpg",
+    nome: "Pluto",
+    tempo: "0,5 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "P",
+    sexo: "Female",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 7,
+    imagem: "/images/dog2.jpg",
+    nome: "Pluto",
+    tempo: "0,5 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "P",
+    sexo: "Female",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
+  {
+    id: 8,
+    imagem: "/images/dog2.jpg",
+    nome: "Pluto",
+    tempo: "0,5 anos",
+    local: "Belo Horizonte/MG",
+    tamanho: "P",
+    sexo: "Female",
+    descricao:
+      "Olá! Meu nome é Billy e estou aqui há 2 anos esperando uma família adotiva. Sou dócil, mas infelizmente nasci sem uma das minhas pernas, por isso preciso de ajuda especial",
+  },
 ];
 
 function Adote_ja() {
-    return (
-            < div className="marketplace" >
-                {animais.map((animal) => (
-                    <Produto key={animal.id} animal={animal} />
-                ))}
-            </div>
-    );
+  return (
+    <div className="background_Image_Adote_ja">
+      <div className="marketplace">
+        {animais.map((animal) => (
+          <Produto key={animal.id} animal={animal} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Adote_ja;
