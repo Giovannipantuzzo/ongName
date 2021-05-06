121fdc636a7fc663386f80cea42ac962b4dc2336
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-05-05 21:23:54 -0300

    Correção de bugs
    bugs nas pags Login, Cadastro e adoção de animal, criados com o merge, resolvidos

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,43 +5,48 @@ import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
 function Login() {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const history = useHistory();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const history = useHistory();
 
-    function login() {
-        alert("Bem vindo!\n" + email);
-        history.push("home");
-    }
-    
-    return(
-        
-        <div className="baseLogin">
-            <div className="containerLogin">
-                <form className="inputs">
-                    <div className="titleConfig"><h1>Login</h1></div>
-                    <Form.Group controlId="email">
-                        <Form.Control 
-                            type="email" 
-                            placeholder="email" 
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                        <Form.Control 
-                            type="password" 
-                            placeholder="Password"
-                            onChange={(e) => setPassword(e.target.value)} 
-                        />
-                    </Form.Group>
-                        <Button className="entrarConfig" variant="dark" onClick={login}>
-                            Entrar
-                            </Button>{' '}
-                    <Link className="linkConfig" to="cadastro">Cadastre-se já</Link>
-                </form>
-            </div>
-        </div>
-    )
+  function login() {
+    alert("Bem vindo!\n" + email);
+    history.push("home");
+  }
+
+  return (
+    <div className="baseLogin">
+      <div className="containerLogin">
+        <form className="inputsLogin">
+          <div className="titleLoginConfig">
+            <h1>Login</h1>
+          </div>
+          <Form.Group controlId="email">
+            <Form.Control
+              type="email"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <div className="entrarConfig2">
+            <Button className="entrarConfig" variant="dark" onClick={login}>
+              Entrar
+            </Button>{" "}
+          </div>
+          <Link className="linkConfig" to="cadastro">
+            Cadastre-se já
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
