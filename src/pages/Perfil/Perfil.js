2cda8009a3911f19c02caf37419f007f3ddd5787
Bruno Campos <brunocampos@cpejr.com.br>
2021-05-14 18:54:05 -0300

    pagina perfil finalizada

--- src/pages/Perfil/Perfil.js
+++ src/pages/Perfil/Perfil.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Form, Col } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import "./Perfil.css";
 
 function Perfil() {
@@ -11,6 +12,12 @@ function Perfil() {
   const [complement, setComplement] = useState();
   const [cellphone, setCellphone] = useState();
   const [birthdate, setBirthdate] = useState();
+  const history = useHistory();
+
+  function saveperfil() {
+    alert("Dados salvos com sucesso!");
+    history.push("home");
+  }
 
   return (
     <div className="basePerfil">
@@ -18,104 +25,93 @@ function Perfil() {
         <div className="titlePerfil">
           <u>Dados Cadastrados</u>
         </div>
-
         <Form className="inputsPerfil">
-          <Form.Row>
-            <Form.Group as={Col} controlId="usernamePerfil">
-              <Form.Label>
-                <b>Username</b>
-              </Form.Label>
-              <Form.Control
-                type="usernamePerfil"
-                placeholder=""
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="cpfPerfil">
-              <Form.Label>
-                <b>CPF</b>
-              </Form.Label>
-              <Form.Control
-                type="cpfPerfil"
-                placeholder=""
-                onChange={(e) => setCpf(e.target.value)}
-              />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Group as={Col} controlId="emailPerfil">
-              <Form.Label>
-                <b>Email</b>
-              </Form.Label>
-              <Form.Control
-                type="emailperfil"
-                placeholder=""
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="cepPerfil">
-              <Form.Label>
-                <b>CEP</b>
-              </Form.Label>
-              <Form.Control
-                type="cepPerfil"
-                placeholder=""
-                onChange={(e) => setCep(e.target.value)}
-              />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Group as={Col} controlId="passwordPerfil">
-              <Form.Label>
-                <b>Password</b>
-              </Form.Label>
-              <Form.Control
-                type="passwordPerfil"
-                placeholder=""
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="complementPerfil">
-              <Form.Label>
-                <b>Complement (house,number,etc)</b>
-              </Form.Label>
-              <Form.Control
-                type="complementPerfil"
-                placeholder=""
-                onChange={(e) => setComplement(e.target.value)}
-              />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Group as={Col} controlId="cellphonePerfil">
-              <Form.Label>
-                <b>Cell Phone Number</b>
-              </Form.Label>
-              <Form.Control
-                type="cellphonePerfil"
-                placeholder=""
-                onChange={(e) => setCellphone(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="birthdatePerfil">
-              <Form.Label>
-                <b>Birth Date</b>
-              </Form.Label>
-              <Form.Control
-                type="birthdatePerfil"
-                placeholder=""
-                onChange={(e) => setBirthdate(e.target.value)}
-              />
-            </Form.Group>
-          </Form.Row>
+          <Form.Group controlId="usernamePerfil">
+            <Form.Label>
+              <b>Username</b>
+            </Form.Label>
+            <Form.Control
+              type="usernamePerfil"
+              placeholder=""
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="cpfPerfil">
+            <Form.Label>
+              <b>CPF</b>
+            </Form.Label>
+            <Form.Control
+              type="cpfPerfil"
+              placeholder=""
+              onChange={(e) => setCpf(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="emailPerfil">
+            <Form.Label>
+              <b>Email</b>
+            </Form.Label>
+            <Form.Control
+              type="emailPerfil"
+              placeholder=""
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="cepPerfil">
+            <Form.Label>
+              <b>CEP</b>
+            </Form.Label>
+            <Form.Control
+              type="cepPerfil"
+              placeholder=""
+              onChange={(e) => setCep(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="passwordPerfil">
+            <Form.Label>
+              <b>Password</b>
+            </Form.Label>
+            <Form.Control
+              type="passwordPerfil"
+              placeholder=""
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="complementPerfil">
+            <Form.Label>
+              <b>Complement (house number,etc)</b>
+            </Form.Label>
+            <Form.Control
+              type="complementPerfil"
+              placeholder=""
+              onChange={(e) => setComplement(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="cellphonePerfil">
+            <Form.Label>
+              <b>Cell Phone Number</b>
+            </Form.Label>
+            <Form.Control
+              type="cellphonePerfil"
+              placeholder=""
+              onChange={(e) => setCellphone(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="birthdatePerfil">
+            <Form.Label>
+              <b>Birth Date</b>
+            </Form.Label>
+            <Form.Control
+              type="birthdatePerfil"
+              placeholder=""
+              onChange={(e) => setBirthdate(e.target.value)}
+            />
+          </Form.Group>
         </Form>
+        <div className="buttonsave2">
+          <Button className="buttonsave" variant="dark" onClick={saveperfil}>
+            Save
+          </Button>
+        </div>
       </div>
     </div>
   );
